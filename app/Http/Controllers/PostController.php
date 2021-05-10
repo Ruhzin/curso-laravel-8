cb6abc03bc8f91a2005b12b9fe1dffb85b2178d1
Ruhzin <55597781+Ruhzin@users.noreply.github.com>
2021-05-10 17:56:42 -0300

    Delete.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,10 +18,35 @@ class PostController extends Controller
     {
         return view('admin.posts.create');
     }
+
     public function store(StoreUpdatePost $request)
     {
         Post::create($request->all());
 
         return  redirect()->route('posts.index');
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+        if (!$post){
+            return redirect()->route('posts.index');
+        }
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        if (!$post){
+            return redirect()->route('posts.index');
+        }
+
+        $post->delete();
+
+        return redirect()
+                        ->route('posts.index')
+                        ->with('success','Post deletado com sucesso !');
     }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,8 +1,17 @@
 <a href="{{ route('posts.create') }}">Criar novo post</a>
 <hr>
 
+@if (session('success'))    
+    <div>
+        {{ session('success') }}
+    </div>
+@endif
+
 <h1>Posts</h1>
 
 @foreach($posts as $post)
-    <p>{{ $post-> title }}</p>
+    <p>
+        {{ $post-> title }}
+        [ <a href="{{ route('posts.show', $post-> id) }}">Ver</a> ]
+    </p>
 @endforeach
