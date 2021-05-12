b6dac94d5d99f99f0c8b1c01a44a58aa9c7608ba
Ruhzin <55597781+Ruhzin@users.noreply.github.com>
2021-05-12 17:23:53 -0300

    instação do breeze

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
 Route::get('/posts/create', [PostController::class, 'create'])-> name('posts.create');
@@ -16,3 +12,15 @@ Route::get('/posts/{id}',[PostController::class, 'show'])->name('posts.show');
 Route::delete('/posts/{id}',[PostController::class, 'destroy'])->name('posts.destroy');
 Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+require __DIR__.'/auth.php';
